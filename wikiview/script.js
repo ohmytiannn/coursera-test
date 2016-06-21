3b55c9b8230a7d1ac2348fb978317e342fe88026
dan <ohmytiannn@gmail.com>
2016-06-21 23:37:25 +0800

    d

--- wikiview/script.js
+++ wikiview/script.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
   $('#random').click(function(){window.open('https://en.wikipedia.org/wiki/Special:Random');})
   $('#searchbutton').click(function(){
+	  
     var input = document.getElementById("frm1");
     input=input.elements[0].value;
     var url = 'https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch='+input +"&format=json&callback=?"; 
@@ -19,12 +20,31 @@ $(document).ready(function(){
      object[0]["animals"][counter]=sub;
      counter=counter+1;
    }
-
+    console.log(object[0])
     var source   = $("#result-template").html();
     var template = Handlebars.compile(source);
     var context = object[0];
     var html    = template(context);
     $("#results").html(html);
+	$('body').append( '<script id="result-template" type="text/x-handlebars-template">')
+	$('body').append('  </script> ')
+      $('#result-template').append('<div class="container">{{#each animals}}<div class="panel panel-info marginbottom">  <div class="panel-heading"><h1><b><a href={{url}}>{{title}}</a></b></h1></div><div class="panel-body"><h4>{{extract}}</h4></div></div> {{/each}}</div>')
+	$('#result-template').append('')
+
+	
+      
+       
+          
+           
+        
+          
+        
+     
+      
+  
+
+ 
+	
  })
 
 });
